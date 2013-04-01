fabd9216db86da7de9626b2ebbcc64ca7b565ecb
Duan Zhiyan <dzy0451@gmail.com>
2013-04-01 14:47:36 +0800

    init

---
+++ slimwriter.php
@@ -0,0 +1,15 @@
+<?php
+
+global $HTMLStylesFmt;
+
+
+$HTMLStylesFmt['pmwiki'] = '
+	    code.escaped { white-space: nowrap; }
+	        .indent { margin-left:40px; }
+		  .outdent { margin-left:40px; text-indent:-40px; }
+		    a.createlinktext { text-decoration:none; border-bottom:1px dotted gray; }
+		      a.createlink { text-decoration:none; position:relative; top:-0.5em;
+    font-weight:bold; font-size:smaller; border-bottom:none; }
+	      img { border:0px; }
+';
+?>
